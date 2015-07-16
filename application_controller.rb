bb4479fc8ca2078325fcb1c08a5cc55359d81c6a
tashaweiner <natashaweiner@gmail.com>
2015-07-16 14:49:24 +0000

    Needs CSS

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,16 @@
 require 'bundler'
 Bundler.require
+require_relative 'models/clothing.rb'
 
 class MyApp < Sinatra::Base
-
-  get '/' do
+  
+  get '/' do #/== www.mywebsite.com/about_me"/" ==website.com
     erb :index
   end
-
+  post '/results' do 
+    @typeshirt=params[:typeshirt]
+    @typepants=params[:typepants]
+    @typeshoes=params[:typeshoes]
+    erb :results
+  end
 end
---
+++ models/clothing.rb
@@ -0,0 +1,9 @@
+def whiteshirt
+  return "/images/whiteshirt.jpg"
+end
+puts
+
+def shirt2
+  return 
+end
+
